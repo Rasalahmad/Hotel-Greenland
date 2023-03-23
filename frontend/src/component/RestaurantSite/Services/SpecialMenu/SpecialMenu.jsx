d18dad2a48a0cef4289fe7bdfa37598ae956b275
Rasalahmad <rasalahmadsit@gmail.com>
2023-03-23 22:57:34 +0600

    wip

--- frontend/src/component/RestaurantSite/Services/SpecialMenu/SpecialMenu.jsx
+++ frontend/src/component/RestaurantSite/Services/SpecialMenu/SpecialMenu.jsx
@@ -1,75 +1,75 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
 const SpecialMenu = () => {
-    return (
-        <div>
-            <Container>
-                <Flex>
-                    <div>
-                        <Title>Main Course</Title>
-                        <Header1>HOT DISHES</Header1>
-                        <div className='grid grid-cols-12'>
-                            <div className=' col-start-1 col-end-3'>
-                                <img
-                                    className="h-24"
-                                    src="http://wahabali.com/work/pearl-demo/images/menu/dish-img1.jpg"
-                                    alt=""
-                                />
-                            </div>
+  return (
+    <div>
+      <Container>
+        <Flex>
+          <div>
+            <Title>Main Course</Title>
+            <Header1>HOT DISHES</Header1>
+            <div className="grid grid-cols-12">
+              <div className=" col-start-1 col-end-3">
+                <img
+                  className="h-24"
+                  src="http://wahabali.com/work/pearl-demo/images/menu/dish-img1.jpg"
+                  alt=""
+                />
+              </div>
 
-                            <Description className=" col-span-9">
+              <Description className=" col-span-9">
+                <div className="flex justify-between lg:w-[480px] ">
+                  <div>
+                    <p className="text-sm md:text-lg  lg:text-lg">
+                      Blue Cheese Crackers with Grapes
+                    </p>
+                  </div>
+                  <div>
+                    <p className="text-sm md:text-xl  lg:text-xl ml-[-220px]">
+                      $12
+                    </p>
+                  </div>
+                </div>
+                <Bottombordar />
+                <Item>Chicken / Grapes / Pizza / Cheese / Herbs</Item>
+              </Description>
+            </div>
+          </div>
 
-                                <div className="flex justify-between lg:w-[480px] ">
-                                    <div>
-                                        <p className="text-sm md:text-lg  lg:text-lg">
-                                            Blue Cheese Crackers with Grapes
-                                        </p>
-                                    </div>
-                                    <div>
-                                        <p className="text-sm md:text-xl  lg:text-xl ml-[-220px]">$12</p>
-                                    </div>
-                                </div>
-                                <Bottombordar />
-                                <Item>Chicken / Grapes / Pizza / Cheese / Herbs</Item>
-                            </Description>
-                        </div>
-                    </div>
+          <div>
+            <Title>Starter</Title>
+            <Header1>BEGINNING</Header1>
+            <div className="grid grid-cols-12">
+              <div className=" col-start-1 col-end-3">
+                <img
+                  className="h-24"
+                  src="http://wahabali.com/work/pearl-demo/images/menu/dish-img1.jpg"
+                  alt=""
+                />
+              </div>
 
-
-
-                    <div>
-                        <Title>Starter</Title>
-                        <Header1>BEGINNING</Header1>
-                        <div className='grid grid-cols-12'>
-                            <div className=' col-start-1 col-end-3'>
-                                <img
-                                    className="h-24"
-                                    src="http://wahabali.com/work/pearl-demo/images/menu/dish-img1.jpg"
-                                    alt=""
-                                />
-                            </div>
-
-                            <Description className=" col-span-9">
-
-                                <div className="flex justify-between lg:w-[480px] ">
-                                    <div>
-                                        <p className="text-sm md:text-lg  lg:text-lg">
-                                            Blue Cheese Crackers with Grapes
-                                        </p>
-                                    </div>
-                                    <div>
-                                        <p className="text-sm md:text-xl  lg:text-xl ml-[-220px]">$12</p>
-                                    </div>
-                                </div>
-                                <Bottombordar />
-                                <Item>Chicken / Grapes / Pizza / Cheese / Herbs</Item>
-                            </Description>
-                        </div>
-                    </div>
-                </Flex>
-            </Container>
-        </div>
-    );
+              <Description className=" col-span-9">
+                <div className="flex justify-between lg:w-[480px] ">
+                  <div>
+                    <p className="text-sm md:text-lg  lg:text-lg">
+                      Blue Cheese Crackers with Grapes
+                    </p>
+                  </div>
+                  <div>
+                    <p className="text-sm md:text-xl  lg:text-xl ml-[-220px]">
+                      $12
+                    </p>
+                  </div>
+                </div>
+                <Bottombordar />
+                <Item>Chicken / Grapes / Pizza / Cheese / Herbs</Item>
+              </Description>
+            </div>
+          </div>
+        </Flex>
+      </Container>
+    </div>
+  );
 };
 
 export default SpecialMenu;
@@ -77,7 +77,7 @@ export default SpecialMenu;
 const Container = styled.div`
   width: 95%;
   margin: auto;
-text-align:center;
+  text-align: center;
   @media (max-width: 768px) {
     width: 100%;
   }
@@ -103,12 +103,12 @@ const Header1 = styled.div`
   }
 `;
 const Flex = styled.div`
-display: flex;
-justify-content: space-around;
-@media (max-width: 768px) {
+  display: flex;
+  justify-content: space-around;
+  @media (max-width: 768px) {
     display: block;
   }
-`
+`;
 const Bottombordar = styled.div`
   border-bottom: solid 1px #cccccc;
   display: block;
@@ -117,7 +117,7 @@ const Bottombordar = styled.div`
 const Description = styled.div`
   font-size: 23px;
   line-height: 40px;
-padding: 3px 35px 35px 35px;
+  padding: 3px 35px 35px 35px;
   text-align: center;
   color: gray;
 `;
