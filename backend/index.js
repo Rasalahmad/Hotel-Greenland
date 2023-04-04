8f9f42014adabf757be602f545a9a2c6e1bcf8b1
Rasalahmad <rasalahmadsit@gmail.com>
2023-04-04 21:01:35 +0600

    wip

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import mongoose from "mongoose";
 import multer from "multer";
+import { SslCommerzPayment } from "sslcommerz";
 import roomsRoute from "./routers/roomsRoute.js";
 import newsRoute from "./routers/newsRoute.js";
 import restaurantRoute from "./routers/restaurantRoute.js";
@@ -59,6 +60,74 @@ app.post("/api/upload", upload.array("images"), (req, res) => {
   const file = req.files;
   res.status(200).json(file.filename);
 });
+
+// ----------- ssl commerz setup -------------
+//sslcommerz init
+app.post("/init", (req, res) => {
+  const data = {
+    total_amount: req.body.room.price,
+    currency: "BDT",
+    product_img: req.body.room?.img,
+    tran_id: "REF123",
+    success_url: "http://hotelgreenlandbd.com/success",
+    fail_url: "http://hotelgreenlandbd.com/fail",
+    cancel_url: "http://hotelgreenlandbd.com/cancel",
+    ipn_url: "http://hotelgreenlandbd.com/ipn",
+    shipping_method: "Courier",
+    product_name: req.body.room.name,
+    product_category: "Electronic",
+    product_profile: "general",
+    cus_name: req?.body?.user?.name,
+    cus_email: req?.body?.user?.email,
+    cus_add1: req.body?.user?.city,
+    cus_add2: "Dhaka",
+    cus_city: "Dhaka",
+    cus_state: "Dhaka",
+    cus_postcode: "1000",
+    cus_country: "Bangladesh",
+    cus_phone: req.body?.user?.phone,
+    cus_fax: req.body?.user?.phone,
+    ship_name: "Customer Name",
+    ship_add1: "Dhaka",
+    ship_add2: "Dhaka",
+    ship_city: "Dhaka",
+    ship_state: "Dhaka",
+    ship_postcode: 1000,
+    ship_country: "Bangladesh",
+    multi_card_name: "mastercard",
+    value_a: "ref001_A",
+    value_b: "ref002_B",
+    value_c: "ref003_C",
+    value_d: "ref004_D",
+  };
+  const sslcommer = new SslCommerzPayment(
+    process.env.STORE_ID,
+    process.env.STORE_PASS,
+    false
+  ); //true for live default false for sandbox
+  sslcommer.init(data).then((data) => {
+    //process the response that got from sslcommerz
+    //https://developer.sslcommerz.com/doc/v4/#returned-parameters
+    if (data.GatewayPageURL) {
+      res.status(200).json(data.GatewayPageURL);
+    }
+    res.status(400).json("Payment seasson failed");
+  });
+});
+
+app.post("/success", async (req, res) => {
+  res.status(200).redirect("http://hotelgreenlandbd.com/success");
+});
+
+app.post("/fail", async (req, res) => {
+  res.status(200).redirect("http://hotelgreenlandbd.com/fail");
+});
+
+app.post("/cancel", async (req, res) => {
+  res.status(200).redirect("http://hotelgreenlandbd.com/cancel");
+});
+
+// --------- ssl commerz setup end -------------
 
 // routing setup
 app.use("/api/room", roomsRoute);
