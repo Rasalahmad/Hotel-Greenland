65eeca387ac4b0efc77e25058a447b02531b46d6
Rasalahmad <rasalahmadsit@gmail.com>
2023-04-08 13:57:19 +0600

    wip

--- Dashboard/src/components/sidebar/Sidebar.jsx
+++ Dashboard/src/components/sidebar/Sidebar.jsx
@@ -21,7 +21,7 @@ const Sidebar = () => {
     <div className="sidebar">
       <div className="top">
         <Link to="/" style={{ textDecoration: "none" }}>
-          <span className="logo">lamadmin</span>
+          <span className="logo">Hotel Greenland</span>
         </Link>
       </div>
       <hr />
