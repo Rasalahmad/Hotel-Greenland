41102be8a922f83acfbb1c905b1d4dd2e08c255d
Rasalahmad <rasalahmadsit@gmail.com>
2023-04-26 23:27:08 +0600

    build

--- Dashboard/src/axios.js
+++ Dashboard/src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const makeRequest = axios.create({
-  baseURL: "https://api.hotelgreenlandbd.com/api",
+  baseURL: "http://api.hotelgreenlandbd.com/api",
   withCredentials: true,
 });
