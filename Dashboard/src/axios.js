af45b3a6ad2051770fbe4828300805c57c7a7203
Rasalahmad <rasalahmadsit@gmail.com>
2023-05-22 00:09:08 +0600

    wip

--- Dashboard/src/axios.js
+++ Dashboard/src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const makeRequest = axios.create({
   baseURL:
-    process.env.PROD === "YES"
+    process.env.REACT_APP_PROD === "YES"
       ? "http://api.hotelgreenlandbd.com/api"
       : "http://localhost:5000/api",
   withCredentials: true,
