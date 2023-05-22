7fc1607d6be1d3f2caf27d8a9dd9ffe72b55497b
Talimul212 <islamtalimul0@gmail.com>
2023-05-22 11:55:53 +0600

    wip

--- backend/routers/restaurantRoute.js
+++ backend/routers/restaurantRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { addFood, getFood ,getSingleFood, updateRestaurant} from "../controllers/restaurantController.js";
+import { addDish, addFood, getFood ,getSingleFood, updateRestaurant} from "../controllers/restaurantController.js";
 
 const router = express.Router();
 
 router.post("/", addFood);
+router.post("/:id", addDish);
 router.get("/", getFood);
 router.get("/:title",getSingleFood);
 router.put("/:id",updateRestaurant);
