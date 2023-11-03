28279f7d2797b961127288a42a2cc3693f0a3e8c
Rasalahmad <rasalahmadsit@gmail.com>
2023-11-03 22:52:20 +0600

    wip

--- frontend/src/component/Success/SuccessView.jsx
+++ frontend/src/component/Success/SuccessView.jsx
@@ -16,11 +16,6 @@ const SuccessView = () => {
     error,
   } = useGetSingleBookingQuery(trans_id);
 
-  console.log(booking?.data, "booking");
-
-  // const { state } = useLocation();
-  // const { paymentMethod, price, roomName, status, bookingDates } =
-  //   state?.booking;
   const lastIndex = booking?.data?.bookingDates?.length - 1;
 
   const handleDownload = (e) => {
@@ -63,63 +58,69 @@ const SuccessView = () => {
               d="M12,0A12,12,0,1,0,24,12,12.014,12.014,0,0,0,12,0Zm6.927,8.2-6.845,9.289a1.011,1.011,0,0,1-1.43.188L5.764,13.769a1,1,0,1,1,1.25-1.562l4.076,3.261,6.227-8.451A1,1,0,1,1,18.927,8.2Z"
             ></path>
           </svg>
-          <div class="text-center">
-            <h3 class="md:text-2xl text-base text-gray-900 font-semibold text-center">
-              Payment Done!
-            </h3>
+          {isLoading ? (
+            "Loading..."
+          ) : (
+            <div class="text-center">
+              <h3 class="md:text-2xl text-base text-gray-900 font-semibold text-center">
+                Payment Done!
+              </h3>
 
-            <div className=" text-left text-lg font-semibold mt-10">
-              <p className="flex justify-between">
-                Transaction ID
-                <span>{booking?.data?.tran_id}</span>
-              </p>
-              <p className="flex justify-between">
-                Payment type
-                <span>{booking?.data?.paymentStatus}</span>
-              </p>
-              <p className="flex justify-between">
-                Amount
-                <span>{booking?.data?.price}</span>
-              </p>
-              <p className="flex justify-between">
-                Room <span>{booking?.data?.product_name}</span>
-              </p>
-              <p className="flex justify-between">
-                Name <span>{booking?.data?.cus_name}</span>
-              </p>
-              <p className="flex justify-between">
-                Booking Dates{" "}
-                <span>
-                  {moment(booking?.data?.bookingDates[0]).format("DD-MM-YYYY")}
-                  {lastIndex > 0 &&
-                    ` to ${moment(
-                      booking?.data?.bookingDates[lastIndex * 1]
-                    ).format("DD-MM-YYYY")}`}
-                </span>
-              </p>
-            </div>
-
-            <div className="py-2 text-center">
-              <div className="flex justify-between">
-                <Link
-                  to="/"
-                  className="px-12 bg-green-400 hover:bg-green-500 rounded-lg text-white font-semibold py-3"
-                >
-                  GO BACK
-                </Link>
-                <button
-                  onClick={handleDownload}
-                  className="px-12 bg-blue-600 hover:bg-blue-500 rounded-lg text-white font-semibold py-3"
-                >
-                  Download Pdf
-                </button>
+              <div className=" text-left text-lg font-semibold mt-10">
+                <p className="flex justify-between">
+                  Transaction ID
+                  <span>{booking?.data?.tran_id}</span>
+                </p>
+                <p className="flex justify-between">
+                  Payment type
+                  <span>{booking?.data?.paymentStatus}</span>
+                </p>
+                <p className="flex justify-between">
+                  Amount
+                  <span>{booking?.data?.price}</span>
+                </p>
+                <p className="flex justify-between">
+                  Room <span>{booking?.data?.product_name}</span>
+                </p>
+                <p className="flex justify-between">
+                  Name <span>{booking?.data?.cus_name}</span>
+                </p>
+                <p className="flex justify-between">
+                  Booking Dates{" "}
+                  <span>
+                    {moment(booking?.data?.bookingDates[0]).format(
+                      "DD-MM-YYYY"
+                    )}
+                    {lastIndex > 0 &&
+                      ` to ${moment(
+                        booking?.data?.bookingDates[lastIndex * 1]
+                      ).format("DD-MM-YYYY")}`}
+                  </span>
+                </p>
               </div>
-              <p className="text-gray-600">
-                Thank you for completing your secure online payment.
-              </p>
-              <p> Have a great day! </p>
+
+              <div className="py-2 text-center">
+                <div className="flex justify-between">
+                  <Link
+                    to="/"
+                    className="px-12 bg-green-400 hover:bg-green-500 rounded-lg text-white font-semibold py-3"
+                  >
+                    GO BACK
+                  </Link>
+                  <button
+                    onClick={handleDownload}
+                    className="px-12 bg-blue-600 hover:bg-blue-500 rounded-lg text-white font-semibold py-3"
+                  >
+                    Download Pdf
+                  </button>
+                </div>
+                <p className="text-gray-600">
+                  Thank you for completing your secure online payment.
+                </p>
+                <p> Have a great day! </p>
+              </div>
             </div>
-          </div>
+          )}
         </div>
       </div>
     </div>
